83387159a5123035b2585db0b6d174c1e7f7080d
Luís Felipe Simões <luis.felipe-simoes@outlook.com>
2024-11-27 19:22:31 -0300

    Implemented search feature on pricing screen

--- src/modules/pricing/screens/PricingList.tsx
+++ src/modules/pricing/screens/PricingList.tsx
@@ -1,25 +1,81 @@
-import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import { PaginationProps } from 'antd';
 import { ColumnsType } from 'antd/es/table';
 import { useEffect, useMemo, useState } from 'react';
+
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
 import Button from '../../../components/button/Button';
 import Modal from '../../../components/modal/Modal';
 import Table from '../../../components/table/Table';
 import usePricingRequests from '../hooks/usePricingRequests';
 import { IPricing } from '../interfaces/PricingInterface';
-
 import { PricingDetails } from './PricingDetails';
 
 export const PricingList = () => {
-  const { getPricing } = usePricingRequests();
+  const { getPricingPaginated } = usePricingRequests();
   const [pricing, setPricing] = useState<IPricing[]>([]);
   const [pricingId, setPricingId] = useState<number>();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const [loading, setLoading] = useState(false);
+  const [search] = useState('');
+  const [current, setCurrent] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [totalItems, setTotalItems] = useState(0);
+
+  const handleEditPricing = (pricing: IPricing) => {
+    if (pricing) {
+      setPricingId(pricing.id);
+    }
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+    setPricingId(undefined);
+  };
+
+  const onChange: PaginationProps['onChange'] = (page) => {
+    setCurrent(page);
+  };
+
+  const onShowSizeChange: PaginationProps['onShowSizeChange'] = (
+    current,
+    pageSize,
+  ) => {
+    setCurrent(current);
+    setItemsPerPage(pageSize);
+  };
+
+  useEffect(() => {
+    if (pricing.length === 0) {
+      loadPricing();
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   useEffect(() => {
     loadPricing();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [current, itemsPerPage, search]);
+
+  const loadPricing = async () => {
+    setLoading(true);
+
+    try {
+      const response = await getPricingPaginated(current, itemsPerPage, search);
+
+      if (response && response.data.length > 0) {
+        setPricing(response.data);
+        setTotalItems(response.meta.totalItems);
+      }
+    } catch (error) {
+      console.log('catch error', error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const columns: ColumnsType<IPricing> = useMemo(
     () => [
@@ -80,23 +136,13 @@ export const PricingList = () => {
     [],
   );
 
-  const handleEditPricing = (pricing: IPricing) => {
-    if (pricing) {
-      setPricingId(pricing.id);
-    }
-    setIsModalOpen(true);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-    setPricingId(undefined);
-  };
-
-  const loadPricing = async () => {
-    const response = await getPricing();
-    if (response) {
-      setPricing(response);
-    }
+  const pagination = {
+    current: current,
+    pageSize: itemsPerPage,
+    total: totalItems,
+    onChange: onChange,
+    onShowSizeChange: onShowSizeChange,
+    showSizeChanger: true,
   };
 
   return (
@@ -112,7 +158,13 @@ export const PricingList = () => {
           />
         </div>
       </div>
-      <Table columns={columns} dataSource={pricing} rowKey='id' />
+      <Table
+        columns={columns}
+        dataSource={pricing}
+        rowKey='id'
+        loading={loading}
+        pagination={pagination}
+      />
       <Modal
         isModalOpen={isModalOpen}
         title='Precificação'
